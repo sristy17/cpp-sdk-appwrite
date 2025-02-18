4ec2a39d879761c98ff792ab2933c69834be17bb
Pooranjoy Bhattacharya <pooranjoyb2016@gmail.com>
2025-02-18 09:22:32 +0000

    feat: getTopic #14
    setup: Messaging module and added the getTopic method

--- include/Appwrite.hpp
+++ include/Appwrite.hpp
@@ -5,6 +5,7 @@
 #include "classes/Databases.hpp"
 #include "classes/Storage.hpp"
 #include "classes/Health.hpp"
+#include "classes/Messaging.hpp"
 
 class Appwrite {
 public:
@@ -13,6 +14,7 @@ public:
     Databases& getDatabases();
     Storage& getStorage();
     Health& getHealth();
+    Messaging& getMessaging();
     
 private:
     std::string projectId;
@@ -21,6 +23,7 @@ private:
     Databases databases;
     Storage storage;
     Health health;
+    Messaging messaging;
 };
 
 #endif
